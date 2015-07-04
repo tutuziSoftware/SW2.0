97f8459796ceec68982fc55ab1396044dd6bdb40
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2015-07-05 02:33:24 +0900

    PCのHPをリファクタリング。技能Lvがセッション中に上昇しても対応できるようにした。

--- jasmine-standalone-2.3.4/src/PlayerCharacter.js
+++ jasmine-standalone-2.3.4/src/PlayerCharacter.js
@@ -3,27 +3,40 @@
  * @constructor
  */
 function PlayerCharacter(json){
-    ["lif", "damage"].forEach(function(name){
+    [
+        "lif",
+        "damage"
+    ].forEach(function(name){
         if(typeof json[name] !== "number") throw 0;
     });
 
+    [
+        "skills"
+    ].forEach(function(name){
+        if(!Array.isArray(json[name])) throw 0;
+    });
+
     var _lif = json.lif;
-    var _lv = json.skills.reduce(function(lv, skill){
-        if(skill.lv > lv){
-            return skill.lv;
-        }else{
-            return lv;
-        }
-    }, 0);
+    var _skills = json.skills;
     var _damage = json.damage;
 
     var self = Object.create(PlayerCharacter.prototype, {
         hp:{
             get:function(){
-                return _lif + _lv * 3 - _damage;
+                return _lif + _lv() * 3 - _damage;
             }
         }
     });
+
+    function _lv(){
+        return _skills.reduce(function(lv, skill){
+            if(skill.lv > lv){
+                return skill.lv;
+            }else{
+                return lv;
+            }
+        }, 0);
+    }
 
     return self;
 };
